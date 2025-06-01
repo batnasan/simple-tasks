1427e70b7cec6850f6332488a5a9502400329f3b
Batnasan Byambasuren <batnasan06@gmail.com>
2025-06-01 12:02:47 +1000

    configuring mongoos with express

--- index.ts
+++ index.ts
@@ -1,13 +1,38 @@
 import 'reflect-metadata';
 
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
 import express, { Express } from 'express';
 import { addRoutes } from './src/config/routes.config';
 
+dotenv.config();
+
 const app: Express = express();
-const port = 3001;
+const port = process.env.PORT;
 
 addRoutes(app);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+async function bootstrap() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Cannot read environment variables');
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      dbName: process.env.DATABASE_NAME,
+    });
+
+    console.log(`Connected to Montodb`);
+
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
